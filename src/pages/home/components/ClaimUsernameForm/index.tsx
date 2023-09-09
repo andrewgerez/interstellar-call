cba2e3693d129c2f96b7670bd5fe93d1631f24ea
Andrew Gerez <andrewgerez18@gmail.com>
2023-09-09 17:29:17 -0300

    feat: hook-form & zod

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,11 +1,29 @@
 import { Button, TextInput } from '@drewdev-ui/react'
-import { Form } from './styles'
+import { useForm } from 'react-hook-form'
 import { ArrowRight } from 'phosphor-react'
+import { Form } from './styles'
+import { z } from 'zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput prefix="interstellar.com/" placeholder="seu-usuario" />
+    <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        prefix="interstellar.com/"
+        placeholder="seu-usuario"
+        {...register('username')}
+      />
       <Button type="submit">
         Reservar
         <ArrowRight />
