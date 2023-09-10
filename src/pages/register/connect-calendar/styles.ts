91bccdfd7f94ed39d1dd664650126d3d78e9e407
Andrew Gerez <andrewgerez18@gmail.com>
2023-09-09 23:16:00 -0300

    feat: connect with google page

---
+++ src/pages/register/connect-calendar/styles.ts
@@ -0,0 +1,19 @@
+import { Box, styled } from '@drewdev-ui/react'
+
+export const ConnectBox = styled(Box, {
+  display: 'flex',
+  flexDirection: 'column',
+  marginTop: '$6',
+})
+
+export const ConnectItem = styled('div', {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+
+  border: '1px solid $gray600',
+  borderRadius: '$md',
+
+  padding: '$4 $6',
+  marginBottom: '$2',
+})
