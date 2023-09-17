6b8a264746fb5e66314eedd7468dbb147b98a093
Andrew Gerez <andrewgerez18@gmail.com>
2023-09-17 17:40:01 -0300

    feat: validation and transformation in zod

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -6,7 +6,7 @@ import {
   Text,
   TextInput,
 } from '@drewdev-ui/react'
-import { Container, Header } from '../styles'
+import { Container, FormError, Header } from '../styles'
 import {
   IntervalBox,
   IntervalDay,
@@ -18,8 +18,26 @@ import { ArrowRight } from 'phosphor-react'
 import { useFieldArray, useForm, Controller } from 'react-hook-form'
 import { z } from 'zod'
 import { getWeekDays } from '../../../utils/get-week-days'
+import { zodResolver } from '@hookform/resolvers/zod'
 
-const timeIntervalsFormSchema = z.object({})
+const timeIntervalsFormSchema = z.object({
+  intervals: z
+    .array(
+      z.object({
+        weekDay: z.number().min(0).max(6),
+        enabled: z.boolean(),
+        startTime: z.string(),
+        endTime: z.string(),
+      }),
+    )
+    .length(7)
+    .transform((intervals) => intervals.filter((interval) => interval.enabled))
+    .refine((intervals) => intervals.length > 0, {
+      message: 'Você precisa selecionar pelo menos um dia da semana.',
+    }),
+})
+
+type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>
 
 export default function TimeIntervals() {
   const {
@@ -29,6 +47,7 @@ export default function TimeIntervals() {
     watch,
     formState: { isSubmitting, errors },
   } = useForm({
+    resolver: zodResolver(timeIntervalsFormSchema),
     defaultValues: {
       intervals: [
         { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
@@ -51,8 +70,8 @@ export default function TimeIntervals() {
 
   const intervals = watch('intervals')
 
-  async function handleSetTimeIntervals() {
-    'teste'
+  async function handleSetTimeIntervals(data: TimeIntervalsFormData) {
+    console.log(data)
   }
 
   return (
@@ -108,7 +127,12 @@ export default function TimeIntervals() {
             )
           })}
         </IntervalsContainer>
-        <Button type="submit">
+
+        {errors.intervals && (
+          <FormError size="sm">{errors.intervals.root?.message}</FormError>
+        )}
+
+        <Button type="submit" disabled={isSubmitting}>
           Próximo passo
           <ArrowRight />
         </Button>
--- src/pages/register/time-intervals/styles.ts
+++ src/pages/register/time-intervals/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from '@drewdev-ui/react'
+import { Box, Text, styled } from '@drewdev-ui/react'
 
 export const IntervalBox = styled(Box, {
   display: 'flex',
@@ -37,4 +37,9 @@ export const IntervalInputs = styled('div', {
   'input::-webkit-calendar-picker-indicator': {
     filter: 'invert(100%) brightness(30%)',
   },
+})
+
+export const FormError = styled(Text, {
+  color: '#f75a68',
+  marginBottom: '$4',
 })
