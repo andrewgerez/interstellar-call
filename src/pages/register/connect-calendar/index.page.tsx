ab30af6254554f65018ebc8a7551d6c8de3c2e57
Andrew Gerez <andrewgerez18@gmail.com>
2023-09-16 23:08:01 -0300

    feat: availability page

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -5,7 +5,7 @@ import { Container, Header } from '../styles'
 import { AuthError, ConnectBox, ConnectItem } from './styles'
 import { useRouter } from 'next/router'
 
-export default function Register() {
+export default function ConnectCalendar() {
   const session = useSession()
   const router = useRouter()
 
